e42ec72b2b57500d5fb6714a69b779c973a525a7
amitajoshigit <91113262+amitajoshigit@users.noreply.github.com>
2022-01-17 13:53:22 +0530

    Update LoginPageTest.java

--- src/test/java/com/observerTesting/testCases/LoginPageTest.java
+++ src/test/java/com/observerTesting/testCases/LoginPageTest.java
@@ -30,7 +30,7 @@ public class LoginPageTest extends TestBase{
 	public void loginPageTitleTest() {
 		String title = loginPage.validateLoginPageTitle();
 		System.out.println("title is "+title);
-		Assert.assertEquals(title, "The Observer | Login");
+		Assert.assertEquals(title, "The Observer");
 	}
 	
 	@Test(priority=2)
